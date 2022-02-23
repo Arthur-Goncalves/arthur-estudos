efda0d9666ceeed56d2ca6d527bc375b138a67c6
Arthur dos Santos Gonçalves - AGC <arthurgon2102@gmail.com>
2022-02-22 21:21:21 -0300

    QANinja Cypress-Discovery - Buger Eats

---
+++ QANinja/cypress-discovery/buger-eats/cypress/integration/home.spec.js
@@ -0,0 +1,10 @@
+
+
+describe('home page',()=>{
+    it('app deve estar online', ()=>{
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+        cy.get('title').should('have.text','Buger Eats')
+        cy.get('h1').should('have.text','Seja um parceiro entregador pela Buger Eats')
+    })
+})
