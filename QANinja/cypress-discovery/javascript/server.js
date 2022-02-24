98d9fdb61eed3a7d8eecea2bb1b1a799c0d7c126
Arthur-Goncalves <arthurgon2102@gmail>
2022-02-24 13:25:17 -0300

    Atualização aulas JS

--- QANinja/cypress-discovery/javascript/server.js
+++ QANinja/cypress-discovery/javascript/server.js
@@ -2,7 +2,32 @@ const express = require('express')
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('Hello Cypress Discovery')
+  res.json({message: 'Hello Cypress Discovery'})
+})
+
+app.get('/avengers', function(req, res) {
+    var avengers = ['Tony Stark', 'Clint Barton', 'Natasha Romanoff', 'Steve Rogers','Bruce Banner','Scot Lang']
+    return res.json({data: avengers})
+})
+
+app.get('/cnh', function(req, res){
+    const idade = req.query.idade
+
+    if (!idade) {
+        return res.json({message: 'Idade é um campo obrigatório.'})
+    }
+
+    var idadeNum = parseInt(idade)
+
+    if (idadeNum >= 18) {
+        return res.json({message: 'Ok, você pode tirar sua CNH.'})
+    } else if (idadeNum > 4) {
+        return res.json({message: 'Você é menor de idade, por enquanto sugiro andar de bike'})
+    } else {
+        return res.json({message: 'Melhor você tomar leite...'})
+    }
+
+    return res.json({test: idade})
 })
 
 app.listen(3000)
