cf9280553cad7e6605630cc6c1aef904ed397478
Arthur-Goncalves <arthurgon2102@gmail>
2022-03-02 17:23:20 -0300

    Refatoração para Inglês com 2 casos de testes

--- QANinja/cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
+++ QANinja/cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -8,46 +8,96 @@ describe('Cadastro', () => {
         cy.get('a[href="/deliver"]').click()
         cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
 
-        var entregador = {
-            nome: 'Arthur Gonçalves',
+        var deliver = {
+            name: 'Arthur Gonçalves',
             cpf: '00000014141',
             email: 'teste@teste.com',
             whatsapp: '11999999999',
-            endereco: {
-                cep: '25960090',
-                rua: 'Rua Gonçalo de Castro',
-                numero: '20',
-                complemento: 'Bloco 5, Ap 105',
-                bairro: 'Alto',
-                cidade_uf: 'Teresópolis/RJ'
+            address: {
+                postalcode: '25960090',
+                street: 'Rua Gonçalo de Castro',
+                number: '20',
+                details: 'Bloco 5, Ap 105',
+                district: 'Alto',
+                city_state: 'Teresópolis/RJ'
             },
-            metodo_entrega: 'Moto',
+            delivery_method: 'Moto',
             cnh: 'cnh-digital.jpg.jpg'
         }
 
-        cy.get('input[name="name"]').type(entregador.nome)
-        cy.get('input[name="cpf"]').type(entregador.cpf)
-        cy.get('input[name="email"]').type(entregador.email)
-        cy.get('input[name="whatsapp"]').type(entregador.whatsapp)
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
 
-        cy.get('input[name="postalcode"]').type(entregador.endereco.cep)
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
         cy.get('input[type="button"][value="Buscar CEP"]').click()
 
-        cy.get('input[name="address-number"]').type(entregador.endereco.numero)
-        cy.get('input[name="address-details"]').type(entregador.endereco.complemento)
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
 
-        cy.get('input[name="address"]').should('have.value', entregador.endereco.rua)
-        cy.get('input[name="district"]').should('have.value', entregador.endereco.bairro)
-        cy.get('input[name="city-uf"]').should('have.value', entregador.endereco.cidade_uf)
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
 
-        cy.contains('.delivery-method li', entregador.metodo_entrega).click()
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
 
         // Upload de arquivo para teste
+        cy.get('input[accept^="image"]').attachFile('/imagens/' + deliver.cnh)
+        cy.get('form button[type="submit"').click()
 
+        const expectedMessage = 'Recebemos os seus dados. Fique de olho na sua caixa de email, pois e em breve retornamos o contato.'
 
+        cy.get('.swal2-container .swal2-html-container').should('have.text', expectedMessage)
 
-      
+    })
 
-    });
-    
-});
+    it('CPF incorreto', () => {
+        cy.viewport(1440, 900)
+        cy.visit('https://buger-eats.vercel.app')
+
+        cy.get('a[href="/deliver"]').click()
+        cy.get('#page-deliver form h1').should('have.text', 'Cadastre-se para  fazer entregas')
+
+        var deliver = {
+            name: 'Arthur Gonçalves',
+            cpf: '000000141AA',
+            email: 'teste@teste.com',
+            whatsapp: '11999999999',
+            address: {
+                postalcode: '25960090',
+                street: 'Rua Gonçalo de Castro',
+                number: '20',
+                details: 'Bloco 5, Ap 105',
+                district: 'Alto',
+                city_state: 'Teresópolis/RJ'
+            },
+            delivery_method: 'Moto',
+            cnh: 'cnh-digital.jpg.jpg'
+        }
+
+        cy.get('input[name="name"]').type(deliver.name)
+        cy.get('input[name="cpf"]').type(deliver.cpf)
+        cy.get('input[name="email"]').type(deliver.email)
+        cy.get('input[name="whatsapp"]').type(deliver.whatsapp)
+
+        cy.get('input[name="postalcode"]').type(deliver.address.postalcode)
+        cy.get('input[type="button"][value="Buscar CEP"]').click()
+
+        cy.get('input[name="address-number"]').type(deliver.address.number)
+        cy.get('input[name="address-details"]').type(deliver.address.details)
+
+        cy.get('input[name="address"]').should('have.value', deliver.address.street)
+        cy.get('input[name="district"]').should('have.value', deliver.address.district)
+        cy.get('input[name="city-uf"]').should('have.value', deliver.address.city_state)
+
+        cy.contains('.delivery-method li', deliver.delivery_method).click()
+
+        // Upload de arquivo para teste
+        cy.get('input[accept^="image"]').attachFile('/imagens/' + deliver.cnh)
+        cy.get('form button[type="submit"').click()
+
+        cy.get('.alert-error').should('have.text', 'Oops! CPF inválido')
+
+    })
+})
