31706a0d944b57740efe281922ec6c01067598c8
Arthur-Goncalves <arthurgon2102@gmail>
2022-03-03 12:05:51 -0300

    Trabalhando com forEach

--- QANinja/cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
+++ QANinja/cypress-discovery/buger-eats/cypress/integration/cadastro.spec.js
@@ -1,5 +1,6 @@
 import signup from '../pages/SignupPage'
 import signupFactory from '../factories/SignupFactory'
+import SignupPage from '../pages/SignupPage'
 
 describe('Cadastro', () => {
 
@@ -44,7 +45,7 @@ describe('Cadastro', () => {
         var deliver = signupFactory.deliver()
 
         deliver.cpf = '000000141aa'
-        
+
         signup.go()
         signup.fillForm(deliver)
         signup.submit()
@@ -62,5 +63,30 @@ describe('Cadastro', () => {
         signup.fillForm(deliver)
         signup.submit()
         signup.alertMessageShouldBe('Oops! Email com formato inválido.')
+    })
+
+    context('Campos Obrigatórios', function () {
+
+        const messages = [
+            { field: 'name', output: 'É necessário informar o nome' },
+            { field: 'cpf', output: 'É necessário informar o CPF' },
+            { field: 'email', output: 'É necessário informar o email' },
+            { field: 'postalcode', output: 'É necessário informar o CEP' },
+            { field: 'number', output: 'É necessário informar o número do endereço' },
+            { field: 'delivery_method', output: 'Selecione o método de entrega' },
+            { field: 'cnh', output: 'Adicione uma foto da sua CNH' }
+
+        ]
+
+        before(function () {
+            SignupPage.go()
+            SignupPage.submit()
+        })
+
+        messages.forEach(function (msg) {
+            it(`${msg.field} is required`, function () {
+                SignupPage.alertMessageShouldBe(msg.output)
+            })
+        })
     })
 })
